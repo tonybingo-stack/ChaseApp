6328ca4ddb1ab265236ca1f0e9baae6e8cdc1857
morsheddev <mmolla@besocial.ai>
2024-07-07 14:25:25 -0400

    Initial Commit

---
+++ ChaseApp/ChaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaseAppApp.swift
+//  ChaseApp
+//
+//  Created by user232392 on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ChaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaseApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChaseApp
+//
+//  Created by user232392 on 7/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
